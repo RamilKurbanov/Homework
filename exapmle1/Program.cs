9770a897a4c95e60260239f0b7864d216768bd8b
Ramil <ramilkurbanov666@gmail.com>
2022-10-17 19:42:09 +0300

    end

---
+++ exapmle1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+    Console.WriteLine("Введите первое число: ");
+            int num_A = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Введите второе число: ");
+            int num_B = int.Parse(Console.ReadLine()!);
+
+            if (num_A > num_B)
+            {
+                Console.WriteLine("Первое число " + num_A + " больше чем второе " + num_B);
+            }
+            else
+            {
+                Console.WriteLine("Второе число " + num_B + " больше чем первое " + num_A);
+            }
